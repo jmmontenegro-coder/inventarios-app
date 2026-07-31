e28acdfe75e06bfe1844ac67c85d97f0c11a1234
Juan Manuel Montenegro <juanmonte2004@gmail.com>
2026-07-30 21:49:03 -0500

    Inyectamos el servicio del producto y obetnemos la lista de productos

--- inventario-app/src/app/producto-lista/producto-lista.ts
+++ inventario-app/src/app/producto-lista/producto-lista.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject, inject } from '@angular/core';
+import { Producto } from '../model/producto.Model';
+import { ProductoServicio } from '../servicios/producto.service';
+import { error } from 'console';
 
 @Component({
   selector: 'app-producto-lista',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   templateUrl: './producto-lista.html'
 })
 export class ProductoLista {
-  
+  productos!: Producto[];
+
+  private productoServicio = inject(ProductoServicio);
+
+  ngOnInit(){
+    //Cargamos los productos
+    this.obtenerProductos();
+
+  }
+
+  private obtenerProductos(): void{
+    this.productoServicio.obtenerProductosLista().subscribe({
+      next: (datos) => {
+        this.productos = datos;
+      },
+      error: (error) => console.error("Errror al obtener los productos", error)
+    });
+  }
 }
--- inventario-app/src/app/servicios/producto.service.ts
+++ inventario-app/src/app/servicios/producto.service.ts
@@ -1,11 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Producto } from '../model/producto.Model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Producto {
+export class ProductoServicio {
   private urlBase = "http://localhost:8080/inventario-app";
 
   constructor(private clienteHttp: HttpClient){}
